4f0e4ab92a8b797d78b002513e6129e9fc7dc654
Anton Aparin <anton@antonaparin.com>
2023-04-09 21:00:33 +0200

    Big Bang 💥

---
+++ src/main.rs
@@ -0,0 +1,64 @@
+use std::{fs, process::exit};
+
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Data {
+    username: String,
+    password: String,
+    hostname: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let filename = "ddns.toml";
+
+    let content = match fs::read_to_string(filename) {
+        Ok(c) => c,
+        Err(e) => {
+            eprintln!("Error while reading file: {}", e);
+            exit(1);
+        }
+    };
+    let data: Data = match toml::from_str(content.as_str()) {
+        Ok(d) => d,
+        Err(e) => {
+            eprintln!("Error while parsing file: {}", e);
+            exit(1);
+        }
+    };
+
+    let mut prev_ip: Option<String> = None;
+
+    loop {
+        let ip = public_ip::addr()
+            .await
+            .unwrap_or_else(|| panic!("Can't get IP-Address"))
+            .to_string();
+
+        println!("IP-Address: {}", ip);
+
+        if *prev_ip.as_ref().unwrap_or(&"".to_owned()) != ip {
+            prev_ip = Some(ip.clone());
+
+            println!("Response: {}", 'print: {
+                if let Ok(response) = reqwest::get(format!(
+                    "https://{}:{}@domains.google.com/nic/update?hostname={}&myip={}",
+                    data.username, data.password, data.hostname, ip
+                ))
+                .await
+                {
+                    if let Ok(text) = response.text().await {
+                        break 'print text;
+                    }
+                }
+
+                "Failed".to_owned()
+            });
+        } else {
+            println!("IP-Address didn't change");
+        }
+
+        std::thread::sleep(std::time::Duration::from_secs(2));
+    }
+}
